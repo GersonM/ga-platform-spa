2874410c0cf675677edb2ea3bb7e1a2cf7395b6f
Gerson A <gersonm17@gmail.com>
2024-03-21 11:34:09 -0500

    add funcionality to index file

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+$path = $_REQUEST['asdasdf']
+$path contains('platform.fbgroup.pe')
+$favionc = 'https://asdfasdfasdf'
+$title = 'FB Group';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/png" href="<?php echo $favionc ?>" />
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600&family=Maven+Pro:wght@500;600&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $title ?></title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
+  </body>
+</html>
