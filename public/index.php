21df06bc879db6e66a6ba287386a5ecdf5ad5247
Gerson A <gersonm17@gmail.com>
2024-03-23 15:30:52 -0500

    update dependencies implement title switcher

--- public/index.php
+++ public/index.php
@@ -1,21 +1,45 @@
 <?php
-$path = $_REQUEST['asdasdf']
-$path contains('platform.fbgroup.pe')
-$favionc = 'https://asdfasdfasdf'
-$title = 'FB Group';
+$tenantNames = array(
+    'platform.fbgroup.pe' => 'FB Group',
+    'fireworks.geekadvice.pe' => 'Fireworks',
+);
+$host = $_SERVER['HTTP_HOST'];
+$dir = scandir('./assets');
+
+$title = 'Geek Advice';
+$jsFile = '';
+$cssFile = '';
+
+if(isset($tenantNames[$host])) {
+  $title = $tenantNames[$host] || 'Geek Advice';
+}
+
+foreach ($dir as $filename) {
+  if (str_contains($filename, 'index')) {
+    if (str_contains($filename, 'css')) {
+      $cssFile = $filename;
+    }
+    if (str_contains($filename, 'js')) {
+      $jsFile = $filename;
+    }
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/png" href="<?php echo $favionc ?>" />
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600&family=Maven+Pro:wght@500;600&display=swap" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php echo $title ?></title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.tsx"></script>
-  </body>
+<head>
+  <meta charset="UTF-8"/>
+  <link rel="icon" type="image/png" href="/assets/logo_square-0cukfj_4.png"/>
+  <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600&family=Maven+Pro:wght@500;600&display=swap"
+        rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title><?php echo $title ?></title>
+  <script type="module" crossorigin src="/assets/<?php echo $jsFile; ?>"></script>
+  <link rel="stylesheet" crossorigin href="/assets/<?php echo $cssFile ?>">
+</head>
+<body>
+<div id="root"></div>
+</body>
 </html>
