b89174f6e8eaeefa4cd4ed228a8b8d560a313278
Gerson A <gersonm17@gmail.com>
2024-08-20 13:37:31 -0500

    hide menu

--- src/MoveManagement/Screens/MoveLayout/index.tsx
+++ src/MoveManagement/Screens/MoveLayout/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Outlet} from 'react-router-dom';
 import {CalendarIcon, MapPinIcon, TicketIcon, TruckIcon, UsersIcon} from '@heroicons/react/24/solid';
 
@@ -6,16 +6,26 @@ import ModuleSidebar from '../../../CommonUI/ModuleSidebar';
 import NavList, {NavListItem} from '../../../CommonUI/NavList';
 import ModuleContent from '../../../CommonUI/ModuleContent';
 import {BsListStars} from 'react-icons/bs';
+import AuthContext from '../../../Context/AuthContext';
 
 const MoveLayout = () => {
+  const {user} = useContext(AuthContext);
   return (
     <>
       <ModuleSidebar title={'Transporte'}>
         <NavList>
           <NavListItem icon={<TicketIcon />} name={'Nueva reserva'} path={'/move/reservation'} />
           <NavListItem icon={<BsListStars />} name={'Mis reservas'} path={'/move/trips'} />
-          <NavListItem icon={<i className={'icon-bus'} />} name={'Vehículos & conductores'} path={'/move/vehicles'} />
-          <NavListItem icon={<MapPinIcon />} name={'Rutas & lugares'} path={'/move/routes'} />
+          {user?.roles?.includes('admin') && (
+            <>
+              <NavListItem
+                icon={<i className={'icon-bus'} />}
+                name={'Vehículos & conductores'}
+                path={'/move/vehicles'}
+              />
+              <NavListItem icon={<MapPinIcon />} name={'Rutas & lugares'} path={'/move/routes'} />
+            </>
+          )}
           <NavListItem icon={<CalendarIcon />} name={'Calendario'} path={'/move/schedule'} />
         </NavList>
       </ModuleSidebar>
