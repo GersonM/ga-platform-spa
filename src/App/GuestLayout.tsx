d38051f6878dbdac0916bff885364d7a99779401
Gerson A. <gersonm17@gmail.com>
2025-07-08 04:13:38 -0500

    fix pexels issue

--- src/App/GuestLayout.tsx
+++ src/App/GuestLayout.tsx
@@ -1,6 +1,6 @@
 import {Suspense, useContext, useEffect, useState} from 'react';
 import {Outlet} from 'react-router-dom';
-import { createClient } from 'pexels';
+//import { createClient } from 'pexels';
 
 import logo from './../Assets/ga_logo.webp';
 import AuthContext from '../Context/AuthContext';
@@ -8,22 +8,22 @@ import Package from '../../package.json';
 import LoadingIndicator from '../CommonUI/LoadingIndicator';
 import './GuestLayout.less';
 const key = 'KcEke7cCtNBvgokFMxxhW2YUuV2AX3rrNVPyIyTo48Dwi7Accj2SRZjm';
-const client = createClient(key);
+//const client = createClient(key);
 
 const GuestLayout = () => {
   const {config, darkMode} = useContext(AuthContext);
   const [bgResource, setBgResource] = useState<string>();
 
-  useEffect(() => {
+  /*useEffect(() => {
     client.photos.random().then((res: any) => {
       setBgResource(res?.src?.landscape);
     })
-  }, []);
+  }, []);*/
 
   const tenantLogo = darkMode ? config?.dark_logo : config?.white_logo;
 
   return (
-    <div className={'login-wrapper'} style={{ backgroundImage: `url(${bgResource})` }}>
+    <div className={'login-wrapper'} >
       <div className="login-block zoom-in">
         <div className="form-block">
           <img src={tenantLogo ? tenantLogo : logo} alt="Logo" className={'logo'} />
