52bcd4484ba6160e3457cf758954e824e02e1495
Gerson A. <gersonm17@gmail.com>
2025-07-13 22:08:52 -0500

    save document

--- src/AccountManagement/Components/CreateUser/index.tsx
+++ src/AccountManagement/Components/CreateUser/index.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {Button, Divider, Form, Input} from 'antd';
+import {Button, Col, Divider, Form, Input, Row, Select} from 'antd';
 import {useForm} from 'antd/lib/form/Form';
 import axios from 'axios';
 
@@ -38,19 +38,38 @@ const CreateUser = ({containerUuid, onCompleted}: CreateUserProps) => {
     <div>
       <h2>Registrar persona</h2>
       <Form form={form} onFinish={submitForm} layout={'vertical'} initialValues={{visibility: 'public'}}>
-        <Form.Item name={'name'} label={'Nombre'} rules={[{required:true}]}>
-          <Input placeholder={'Nombre'} />
+        <Form.Item name={'name'} label={'Nombre'} rules={[{required: true}]}>
+          <Input placeholder={'Nombre'}/>
         </Form.Item>
-        <Form.Item name={'last_name'} label={'Apellidos'} rules={[{required:true}]}>
-          <Input placeholder={'Nombre'} />
+        <Form.Item name={'last_name'} label={'Apellidos'} rules={[{required: true}]}>
+          <Input placeholder={'Apellidos'}/>
         </Form.Item>
+        <Row gutter={15}>
+          <Col span={8}>
+            <Form.Item name={'doc_type'} label={'Tipo de documento'}>
+              <Select
+                placeholder={'DNI'}
+                options={[
+                  {label: 'DNI', value: 'dni'},
+                  {label: 'CE', value: 'ce'},
+                  {label: 'Pasaporte', value: 'passport'},
+                ]}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={16}>
+            <Form.Item name={'doc_number'} label={'Número de documento'}>
+              <Input placeholder={'Número'}/>
+            </Form.Item>
+          </Col>
+        </Row>
         <Divider>Crear cuenta</Divider>
         <p>Estos datos on opcionales, si especificas una contraseña esta persona podrá iniciar sesión en el sistema</p>
         <Form.Item name={'email'} label={'E-mail'}>
-          <Input placeholder={'E-mail'} />
+          <Input placeholder={'E-mail'}/>
         </Form.Item>
         <Form.Item name={'password'} label={'Contraseña'}>
-          <Input.Password placeholder={'Contraseña'} />
+          <Input.Password placeholder={'Contraseña'}/>
         </Form.Item>
         <Button block shape={'round'} loading={loading} type={'primary'} htmlType={'submit'}>
           Crear usuario
