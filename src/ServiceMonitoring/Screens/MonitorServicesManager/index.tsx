5898724a0ceb35d8c9bbcbc2b102036b94821310
Gerson A. <gersonm17@gmail.com>
2025-10-28 17:04:16 -0500

    lis services

--- src/ServiceMonitoring/Screens/MonitorServicesManager/index.tsx
+++ src/ServiceMonitoring/Screens/MonitorServicesManager/index.tsx
@@ -19,6 +19,7 @@ import ModalView from "../../../CommonUI/ModalView";
 import ProductForm from "../../../WarehouseManager/Components/ProductForm";
 import ProductStockManager from "../../../WarehouseManager/Components/ProductStockManager";
 import WarehouseManager from "../../../WarehouseManager/Components/WarehouseManager";
+import CustomTag from "../../../CommonUI/CustomTag";
 
 const MonitorServicesManager = () => {
   const [loading, setLoading] = useState(false);
@@ -58,14 +59,32 @@ const MonitorServicesManager = () => {
 
   const columns = [
     {
-      title: 'Acción',
-      dataIndex: 'status',
+      title: 'ID',
+      dataIndex: 'uuid',
       align: 'center',
       width: 100,
     },
     {
+      title: 'Acción',
+      dataIndex: 'status',
+      align: 'center',
+      width: 100,
+      render: (text: string) => (<CustomTag color={'green'}>{text}</CustomTag>)
+    },
+    {
       title: 'IP',
       dataIndex: 'ip',
+      render: (text: string) => (<code>{text}</code>)
+    },
+    {
+      title: 'Procesador',
+      dataIndex: 'uuid',
+      render: (text: string) => (
+        <>
+          AMD Ryzen 9 3900X 12-Core Processor
+          <small>DDR4 3200MHz 64GB (2 x 32GB 3200MHz)</small>
+        </>
+      )
     },
     {
       title: 'Disco total',
@@ -97,16 +116,39 @@ const MonitorServicesManager = () => {
   const data = [
     {
       status: 'online',
-      uuid: "74.50.77.162",
+      uuid: "223596",
       ip: "74.50.77.162",
       name: "SV1",
-      disk: "2Tb",
+      features: "AMD Ryzen 9 3900X 12-Core Processor",
+      disk: "Samsung SSD 970 EVO Plus 2TB",
       available_disk: "213Gb",
       uptime: "12 días",
       created_at: "07-01-2025",
     },
     {
-      status: 'expired',
+      status: 'online',
+      uuid: "234csf",
+      ip: "66.45.233.122",
+      features: 'AMD Ryzen 5 3600X 6-Core Processor',
+      memory: "DDR4 3200MHz 64GB (2 x 32GB 3200MHz)",
+      name: "SV2",
+      disk: "Crucial 1TB NVMe SN:2243E67E10C9 - Samsung SSD 970 EVO Plus 1TB SN:S59ANM0RC00743X",
+      available_disk: "213Gb",
+      uptime: "12 días",
+      created_at: "06-21-2025",
+    },
+    {
+      status: 'online',
+      uuid: "234csf",
+      ip: "74.50.75.242",
+      name: "SV2",
+      disk: "2Tb",
+      available_disk: "213Gb",
+      uptime: "12 días",
+      created_at: "06-21-2025",
+    },
+    {
+      status: 'online',
       uuid: "234csf",
       ip: "66.45.233.122",
       name: "SV2",
@@ -114,7 +156,27 @@ const MonitorServicesManager = () => {
       available_disk: "213Gb",
       uptime: "12 días",
       created_at: "06-21-2025",
-    }
+    },
+    {
+      status: 'online',
+      uuid: "134642",
+      ip: "66.45.225.18",
+      name: "SV2",
+      disk: "2Tb",
+      available_disk: "213Gb",
+      uptime: "12 días",
+      created_at: "06-21-2025",
+    },
+    {
+      status: 'online',
+      uuid: "105157",
+      ip: "209.159.145.122",
+      name: "SV2",
+      disk: "2Tb",
+      available_disk: "213Gb",
+      uptime: "12 días",
+      created_at: "06-21-2025",
+    },
   ]
 
   return (
@@ -122,34 +184,28 @@ const MonitorServicesManager = () => {
       <ContentHeader
         title={'Monitoreo de servicios'}
         loading={loading}
+        largeTools
         onRefresh={() => setReload(!reload)}
-        tools={<Space>
-          <Tooltip title={'Gestionar almacenes'}>
-            <IconButton icon={<PiShippingContainer/>} onClick={() => setOpenWarehouseManager(true)}/>
-          </Tooltip>
-        </Space>}
-        onAdd={() => setOpenAddProduct(true)}/>
-      <FilterForm
-        onInitialValues={values => setFilters(values)}
-        onSubmit={values => setFilters(values)}
-      >
-        <Form.Item name={'search'} label={'Buscar'}>
-          <Input.Search
-            allowClear
-            placeholder={'Buscar por nombre, código o descripción'}
-          />
-        </Form.Item>
-        <Form.Item name={'group'} label={'Grupo'}>
-          <ProductGroupsSelector/>
-        </Form.Item>
-        <Form.Item name={'brand'}>
-          <ProductBrandSelector/>
-        </Form.Item>
-        <Form.Item name={'manufacturer'}>
-          <ProductManufacturerSelector/>
-        </Form.Item>
-      </FilterForm>
-      <TableList columns={columns} dataSource={data}/>
+        onAdd={() => setOpenAddProduct(true)}>
+        <FilterForm
+          onInitialValues={values => setFilters(values)}
+          onSubmit={values => setFilters(values)}
+        >
+          <Form.Item name={'search'} label={'Buscar'}>
+            <Input.Search
+              allowClear
+              placeholder={'Buscar por nombre, código o descripción'}
+            />
+          </Form.Item>
+          <Form.Item name={'ip'} label={'IP'}>
+            <ProductGroupsSelector/>
+          </Form.Item>
+          <Form.Item name={'manufacturer'}>
+            <ProductManufacturerSelector/>
+          </Form.Item>
+        </FilterForm>
+      </ContentHeader>
+      <TableList customStyle={false} columns={columns} dataSource={data}/>
       {pagination && (
         <Pagination
           showSizeChanger={false}
